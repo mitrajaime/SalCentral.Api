1604f573f04df98c53585f001e4b8a166f1e9c1d
Stanley <stanleyclintonso@gmail.com>
2024-10-28 21:41:25 +0800

    SI AKASHI

--- SalCentral.Api/Program.cs
+++ SalCentral.Api/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddDbContext<ApiDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("LocalConnection")));
+builder.Services.AddDbContext<ApiDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("stanleyconnection")));
 
 builder.Services.AddCors(options =>
 {
